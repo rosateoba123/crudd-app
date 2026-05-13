954d2f4571b08558753a6f694e1b3dc0103b104e
Rosa <rosa.teoba123@gmail.com>
2026-05-12 19:20:10 -0600

    Configuracion Render

--- backend/server.js
+++ backend/server.js
@@ -68,6 +68,8 @@ app.delete("/users/:id", (req, res) => {
   });
 });
 
-app.listen(3000, () => {
-  console.log("Servidor en http://localhost:3000");
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Servidor en puerto ${PORT}`);
 });
